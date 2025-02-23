1ae3a686be46ecfe9d54d1c3d1874bb9e02e2cf7
iamSAGARSONI <itissagarsoni@gmail.com>
2025-02-23 11:25:49 +0530

    edited the page

--- app/components/features.tsx
+++ app/components/features.tsx
@@ -23,14 +23,19 @@ export default function Features() {
     ];
   
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5  ">
         {features.map((feature) => (
           <div
             key={feature.id}
-            className="bg-gray-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow"
+            className="flex flex-col justify-center text-center items-center  bg-gray-800 p-6 rounded-lg shadow-lg h-[300px] hover:shadow-xl transition-shadow "
           >
+            <div className="col-12">
             <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
+            </div>
+            
+            <div className="col-12">
             <p className="text-gray-300">{feature.description}</p>
+            </div>
           </div>
         ))}
       </div>
